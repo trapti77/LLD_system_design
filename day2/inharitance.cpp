f0147181975f018e9bc01d2bb46ccbf59b9492de
trapti patel <traptipatel46@gmail.com>
2025-08-19 21:52:49 +0530

    main

---
+++ day2/inharitance.cpp
@@ -0,0 +1,27 @@
+/*
+We know that real world Objects show inheritance relationship where we
+have parent object and child object. child object have all the characters
+or behaviours that parent have plus some additional characters/behaviours.
+Like all cars in real world have a brand, model etc and can start, stop,
+accelerate etc. But some specific cars like manual car have gear System
+while other specific cars like Electric cars have battery system.
+
+We represent this scenario of real world in programming by creating a parent class and
+defining all the characters(variables) or behaviours(methods) that all cars
+have in parent class. Then we create different child classes that inherits
+from this parent class and define only those characters and behaviours
+that are specific to them. Although objects of these child classes can
+access or call parent class characters(variables) and behaviours(methods).
+Hence providing code reusability.
+*/
+
+//---------------------------------INHARITANCE---------------
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
