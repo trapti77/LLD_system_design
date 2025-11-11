06b1af21aefa2585556f7b9080ad24271e81b8bb
trapti patel <traptipatel46@gmail.com>
2025-11-11 23:33:20 +0530

    zomato project start

---
+++ project/zomato/model/Cart.h
@@ -0,0 +1,63 @@
+#ifndef CART_H
+#define CART_H
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include "../model/MenuItem.h"
+#include "../model/Restaurant.h"
+
+using namespace std;
+
+class Cart
+{
+private:
+    Restaurant* restaurant;
+    vector<MenuItem> items;
+
+public:
+    Cart(){
+        restaurant = nullptr;
+    }
+    void addItem(const MenuItem* item){
+        if(!restaurant){
+            cerr << "Cart: Set a restaurant before adding items." << endl;
+            return;
+        }
+        items.push_back(item);
+    }
+    double getTotalCost()const{
+        double sum = 0;
+        for(const auto& it:items){
+            sum += it.getPrice();
+        }
+    }
+    bool isEmpty()
+    {
+        return (!restaurant || items.empty());
+    }
+
+    void clear()
+    {
+        items.clear();
+        restaurant = nullptr;
+    }
+
+    // Getters and Setters
+    void setRestaurant(Restaurant *r)
+    {
+        restaurant = r;
+    }
+
+    Restaurant *getRestaurant() const
+    {
+        return restaurant;
+    }
+
+    const vector<MenuItem> &getItems() const
+    {
+        return items;
+    }
+};
+
+#endif
---
+++ project/zomato/model/User.h
@@ -0,0 +1,55 @@
+#ifndef USER_H
+#define USER_H
+
+#include<string>
+#include "../model/Cart.h"
+
+using namespace std;
+
+class User
+{
+private:
+    int userId;
+    string name;
+    string address;
+    Cart *cart;
+
+public:
+    User(int userId, const string &name, const string &address)
+    {
+        this->userId = userId;
+        this->name = name;
+        this->address = address;
+        Cart *cart = new Cart();
+    }
+        ~User(){
+            delete cart;
+        }
+        // Getters and Setters
+        string getName() const
+        {
+            return name;
+        }
+
+        void setName(const string &n)
+        {
+            name = n;
+        }
+
+        string getAddress() const
+        {
+            return address;
+        }
+
+        void setAddress(const string &a)
+        {
+            address = a;
+        }
+        Cart *getCart() const
+        {
+            return cart;
+        }
+    };
+
+
+#endif
